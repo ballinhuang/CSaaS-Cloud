c11bdc8b1b3fd243613c6ef5b8a9b17ffd3ae573
ballinhuang <x3562151@gmail.com>
2017-07-26 00:34:46 +0800

    7/26

--- Client/Index/Component/Menu.vue
+++ Client/Index/Component/Menu.vue
@@ -1,3 +1,4 @@
+<!--
 <template>
     <div>
         <nav>
@@ -38,3 +39,57 @@ export default {
     }
 }
 </script>
+-->
+<template>
+    <v-app id="example-2" standalone>
+        <v-navigation-drawer permanent light>
+            <v-toolbar flat class="transparent">
+                <v-list class="pa-0">
+                    <v-list-tile avatar tag="ul">Test</v-list-tile>
+                </v-list>
+            </v-toolbar>
+            <v-divider></v-divider>
+            <v-list dense class="pt-0">
+                <v-list-tile v-for="item in items" :key="item.title" :href="item.href" :router="item.router">
+                    <v-list-tile-action>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-tile-action>
+                    <v-list-tile-content>
+                        <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+            </v-list>
+        </v-navigation-drawer>
+        <v-toolbar class="cyan" dark>
+            <v-toolbar-title>Toolbar</v-toolbar-title>
+        </v-toolbar>
+        <main>
+            <v-container fluid>
+                <router-view></router-view>
+            </v-container>
+        </main>
+    </v-app>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            items: [{
+                href: '#/home',
+                router: true,
+                title: 'Home',
+                icon: 'home',
+            }, {
+                href: '#/profile',
+                router: true,
+                title: 'Profile',
+                icon: 'extension',
+            }]
+        }
+    },
+    methods: {
+    }
+}
+</script>
+
--- Client/Index/Index.js
+++ Client/Index/Index.js
@@ -2,13 +2,17 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 import VueHead from 'vue-head'
+import Vuetify from 'vuetify'
+
 import HomeView from './Component/Home.vue'
 import ProfileView from './Component/Profile.vue'
 import Menu from './Component/Menu.vue'
 
+Vue.use(VueHead)
 Vue.use(VueRouter)
 Vue.use(VueHead)
-Vue.use(VueResource)
+Vue.use(Vuetify)
+
 const routes = [
     { path: '/', component: HomeView },
     { path: '/home', component: HomeView },
