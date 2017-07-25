fb4194958a84833dc677cb8df3b510540490e80c
ballinhuang <x3562151@gmail.com>
2017-07-26 01:21:56 +0800

    Vuetify

--- Client/Index/Component/Menu.vue
+++ Client/Index/Component/Menu.vue
@@ -1,55 +1,15 @@
-<!--
 <template>
-    <div>
-        <nav>
-            <div class="nav-wrapper cyan">
-                <div class="container">
-                    <a class="brand-logo">Menu</a>
-                    <ul id="nav-mobile" class="right hide-on-med-and-down">
-                        <li>
-                            <router-link to="/home">Home</router-link>
-                        </li>
-                        <li>
-                            <router-link to="/profile">{{UserName}}</router-link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <router-view></router-view>
-    </div>
-</template>
-
-<script>
-import API from '../../WebAPI.js'
-export default {
-    data () {
-        return {
-            UserName: ""
-        }
-    },
-    methods: {
-    },
-    created: function () {
-        API.getUserName((res) => {
-            this.UserName = res.body.name;
-        }, (res) => {
-            this.UserName = "";
-        })
-    }
-}
-</script>
--->
-<template>
-    <v-app id="example-2" standalone>
-        <v-navigation-drawer permanent light>
-            <v-toolbar flat class="transparent">
-                <v-list class="pa-0">
-                    <v-list-tile avatar tag="ul">Test</v-list-tile>
-                </v-list>
-            </v-toolbar>
-            <v-divider></v-divider>
-            <v-list dense class="pt-0">
+    <v-app standalone>
+        <v-navigation-drawer persistent v-model="drawer" light enable-resize-watcher overflow absolute>
+            <v-list>
+                <v-list-tile>
+                    <v-list-tile-content>
+                        <v-list-tile-title>
+                            <span>Menu</span>
+                        </v-list-tile-title>
+                    </v-list-tile-content>
+                </v-list-tile>
+                <v-divider></v-divider>
                 <v-list-tile v-for="item in items" :key="item.title" :href="item.href" :router="item.router">
                     <v-list-tile-action>
                         <v-icon>{{ item.icon }}</v-icon>
@@ -60,14 +20,18 @@ export default {
                 </v-list-tile>
             </v-list>
         </v-navigation-drawer>
-        <v-toolbar class="cyan" dark>
-            <v-toolbar-title>Toolbar</v-toolbar-title>
+        <v-toolbar class="indigo" dark>
+            <v-toolbar-side-icon @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-title>Dashboard</v-toolbar-title>
         </v-toolbar>
         <main>
             <v-container fluid>
                 <router-view></router-view>
             </v-container>
         </main>
+        <v-footer class="indigo">
+            <span class="white--text">© 2017 NTCU</span>
+        </v-footer>
     </v-app>
 </template>
 
@@ -75,6 +39,8 @@ export default {
 export default {
     data () {
         return {
+            drawer: true,
+
             items: [{
                 href: '#/home',
                 router: true,
@@ -87,9 +53,6 @@ export default {
                 icon: 'extension',
             }]
         }
-    },
-    methods: {
     }
 }
 </script>
-
