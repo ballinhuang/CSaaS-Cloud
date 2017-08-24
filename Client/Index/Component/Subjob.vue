dc56934dfc171da72269116a78ac6c05eaa8c44c
ballinhuang <x3562151@gmail.com>
2017-08-24 15:21:19 +0800

    8/24 MPI checkbox add

--- Client/Index/Component/Subjob.vue
+++ Client/Index/Component/Subjob.vue
@@ -1,20 +1,21 @@
 <template>
     <v-layout row>
-        <v-dialog v-model="dialog" persistent>
-            <v-btn primary dark slot="activator">Subjob</v-btn>
+        <v-dialog v-model="dialog" width="800px" persistent>
+            <v-btn primary dark slot="activator">Job Submission</v-btn>
             <v-card>
                 <v-card-title>
-                    <span class="headline">Subjob</span>
+                    <span class="headline">Job Submission</span>
                 </v-card-title>
                 <v-card-text>
                     <v-text-field label="Job name" class="mt-5" v-model="filename"></v-text-field>
                     <v-text-field label="Write the scipt" multi-line v-model="script"></v-text-field>
-                    <v-select label="Select nodes need" persistent-hint :items="nodeoptions" v-model="nodeneed"></v-select>
-                    <v-select label="Select cpu need" persistent-hint :items="cpuoptions" v-model="cpuneed"></v-select>
+                    <v-checkbox v-bind:label="`MPI: ${ex1.toString()}`" v-model="ex1" light></v-checkbox>
+                    <v-select v-show="ex1" label="Select nodes needed" persistent-hint @change="initcpuoptions" :items="nodeoptions" v-model="nodeneed"></v-select>
+                    <v-select v-show="ex1" label="Cpu per node" persistent-hint :items="cpuoptions" v-model="cpuneed"></v-select>
                 </v-card-text>
                 <v-card-actions>
                     <v-spacer></v-spacer>
-                    <v-btn class="blue--text darken-1" flat @click.native="sendjob">Send</v-btn>
+                    <v-btn class="blue--text darken-1" flat @click.native="sendjob">Submit</v-btn>
                     <v-btn class="blue--text darken-1" flat @click.native="cleanfield">Close</v-btn>
                 </v-card-actions>
             </v-card>
@@ -32,15 +33,20 @@ export default {
             nodeoptions: [],
             cpuoptions: [],
             maxcpu: 0,
+            totalcpu: 0,
             cpuneed: 1,
             nodeneed: 0,
             filename: '',
-            script: ''
+            script: '',
+            ex1: false
         }
     },
     methods: {
         sendjob () {
             this.script += "\n#SET -N " + this.nodeneed + " -P " + this.cpuneed
+            if (this.ex2 === true) {
+                this.script += "\n#MPI"
+            }
             var msg = {
                 name: this.cluster.name,
                 port: this.cluster.port,
@@ -50,12 +56,10 @@ export default {
                 script: this.script
             }
             API.subjob(msg, (res) => {
-                //alert(res.body.msg)
                 this.alertmsg.msg = res.body.msg
                 this.alertmsg.alert = true
                 this.alertmsg.type = "success"
             }, (res) => {
-                //alert(res.body)
                 this.alertmsg.msg = res.body
                 this.alertmsg.alert = true
                 this.alertmsg.type = "error"
@@ -64,25 +68,41 @@ export default {
         },
         cleanfield () {
             this.dialog = false
-            this.cpuneed = 0
+            this.cpuneed = 1
             this.nodeneed = 0
             this.filename = ''
             this.script = ''
+            this.ex1 = false
+            this.ex2 = false
+        },
+        initcpuoptions () {
+            this.cpuoptions = [];
+            var i = 1;
+            if (this.nodeneed !== 0) {
+                for (i = 1; i <= this.maxcpu; i++) {
+                    this.cpuoptions.push({ value: i, text: i });
+                }
+            }
+            else {
+                for (i = 1; i <= this.totalcpu; i++) {
+                    this.cpuoptions.push({ value: i, text: i });
+                }
+            }
         }
     },
     created: function () {
         var i = 0;
-        for (i = 0; i <= parseInt(this.cluster.nodes); i++) {
+        this.nodeoptions.push({ value: i, text: 'Free' });
+        for (i = 1; i <= parseInt(this.cluster.nodes); i++) {
             this.nodeoptions.push({ value: i, text: i });
         }
         for (var node in this.cluster.nodeslist) {
+            this.totalcpu += parseInt(this.cluster.nodeslist[node].nodenp)
             if (parseInt(this.cluster.nodeslist[node].nodenp) > this.maxcpu) {
                 this.maxcpu = parseInt(this.cluster.nodeslist[node].nodenp)
             }
         }
-        for (i = 1; i <= this.maxcpu; i++) {
-            this.cpuoptions.push({ value: i, text: i });
-        }
+        this.initcpuoptions()
     },
     props: ['cluster', 'alertmsg']
 }
