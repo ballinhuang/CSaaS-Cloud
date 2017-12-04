844853732d335a23bcf632fc5920438a9a2fd330
ballinhuang <x3562151@gmail.com>
2017-12-05 02:59:09 +0800

    12/5 sim data table

--- Client/Index/Component/Simulator/Result.vue
+++ Client/Index/Component/Simulator/Result.vue
@@ -10,9 +10,22 @@
         </v-card-title>
         <v-divider></v-divider>
           <v-card-text style="height: 800px;">
-              <p v-for="d in data" :key="d">
-                {{d}}
-              </p>
+            <v-data-table
+              v-if="dialog"
+              v-bind:headers="headers"
+              :items="results"
+              hide-actions
+              class="elevation-1"
+            >
+              <template slot="items" scope="props">
+                <td class="text-xs-center">{{ props.item.jobid }}</td>
+                <td class="text-xs-center">{{ props.item.subt }}</td>
+                <td class="text-xs-center">{{ props.item.runt }}</td>
+                <td class="text-xs-center">{{ props.item.waitt }}</td>
+                <td class="text-xs-center">{{ props.item.np }}</td>
+                <td class="text-xs-center">{{ props.item.order }}</td>
+              </template>
+            </v-data-table>
           </v-card-text>
         <v-divider></v-divider>
         <v-card-actions>
@@ -27,8 +40,33 @@
 export default {
   data() {
     return {
-      dialog: false
+      dialog: false,
+      results: [],
+      headers: [
+        { text: "Job ID", value: "jobid", align: "center" },
+        { text: "Submit time", value: "subt", align: "center" },
+        { text: "Run time", value: "runt", align: "center" },
+        { text: "Wait time", value: "waitt", align: "center" },
+        { text: "NP", value: "np", align: "center" },
+        { text: "Order", value: "order", align: "center" }
+      ]
     };
+  },
+  watch: {
+    data: function() {
+      this.results = [];
+      for (var i in this.data) {
+        const row = this.data[i].split(" ");
+        this.results.push({
+          jobid: row[1],
+          subt: row[2],
+          runt: row[3],
+          waitt: row[4],
+          np: row[5],
+          order: row[6]
+        });
+      }
+    }
   },
   props: ["data"]
 };
